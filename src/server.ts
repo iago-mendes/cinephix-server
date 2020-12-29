6726d1626a0a5dd656d89c05efd1fd33581c4a0e
Iago <iagobrazmendes@gmail.com>
2020-12-29 18:21:23 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'express-async-errors'
+import dotenv from 'dotenv'
+
+import routes from './routes'
+import errorHandler from './errors/handler'
+
+const app = express()
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(routes)
+app.use(errorHandler)
+
+const port = 2001
+app.listen(port, () => console.log('server started at port', port))
