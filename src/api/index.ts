36512ef53b54607a8d2fbf7bc7067f9801d64fe6
Iago <iagobrazmendes@gmail.com>
2020-12-29 19:22:00 -0300

    connect to tmdb

---
+++ src/api/index.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const api = axios.create(
+{
+	baseURL: 'https://api.themoviedb.org/3/',
+	params:
+	{
+		api_key: process.env.TMDB_KEY
+	}
+})
+
+export default api
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,19 @@
 import express from 'express'
 
+import api from './api'
+
 const routes = express.Router()
 
 routes.get('/', (req, res) => res.json({message: 'Hello world!'}))
+
+routes.get('/test', async (req, res) =>
+{
+	const params = {query: 'avengers'}
+
+	// const {data: test} = await api.get('/movie/550')
+	const {data: test} = await api.get('/search/movie', {params})
+
+	return res.json(test)
+})
 
 export default routes
