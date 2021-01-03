2ed11b644176ef087f46eed156ab94a751731a31
Iago <iagobrazmendes@gmail.com>
2021-01-03 17:28:31 -0300

    get interface of movie details

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -49,3 +49,58 @@ export interface MovieTrendingPaginated
 	total_results: number
 	total_pages: number
 }
+
+interface MovieDetails
+{
+	adult?: boolean
+	backdrop_path?: string
+	belongs_to_collection?: object
+	budget?: number
+	genres?: Array<
+	{
+		id?: number
+		name?: string
+	}>
+	homepage?: string
+	id?: number
+	imdb_id?: string
+	original_language?: string
+	original_title?: string
+	overview?: string
+	popularity?: number
+	poster_path?: string
+	production_companies?: Array<
+	{
+		name?: string
+		id?: number
+		logo_path?: string
+		origin_country?: string
+	}>
+	production_countries?: Array<
+	{
+		iso_3166_1?: string
+		name?: string
+	}>
+	release_date?: string
+	revenue?: number
+	runtime?: number
+	spoken_languages?: Array<
+	{
+		iso_639_1?: string
+		name?: string
+	}>
+	status?: string
+	tagline?: string
+	title?: string
+	video?: boolean
+	vote_average?: number
+	vote_count?: number
+}
+
+export interface MovieDetailsPaginated
+{
+	page: number
+	results: MovieDetails[]
+	total_results: number
+	total_pages: number
+}
