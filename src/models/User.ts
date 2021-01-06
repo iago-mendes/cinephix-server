5d50e03c071b5fe8c13bffe7d3410be7846bac3b
Iago <iagobrazmendes@gmail.com>
2021-01-06 11:51:44 -0300

    improve user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,60 +5,78 @@ export type UserType = mongoose.Document &
 	_id?: string
 	email: string
 	joinedAt?: Date
-	ratings:
+	tvshows: Array<
 	{
-		movies: Array<
-		{
-			movieId: number
+		tvshowId: number
 
+		status?: string
+		venue?: string
+
+		ratings?:
+		{
+			engagement?: number
+			consistency?: number
+			screenplay?: number
+			acting?: number
+			cinematography?: number
+			musicAndSound?: number
+		}
+	}>
+	movies: Array<
+	{
+		movieId: number
+
+		status: string
+		venue: string
+
+		ratings:
+		{
 			screenplay: number
 			pacing: number
 			acting: number
 			cinematography: number
 			musicAndSound: number
-		}>
-		tvshows: Array<
-		{
-			tvshowId: number
-
-			engagement: number
-			consistency: number
-			screenplay: number
-			acting: number
-			cinematography: number
-			musicAndSound: number
-		}>
-	}
+		}
+	}>
 }
 
 const UserSchema = new mongoose.Schema(
 {
 	email: {type: String, required: true, unique: true},
 	joinedAt: {type: Date, default: Date.now()},
-	ratings:
-	{
-		movies:
-		[{
-			movieId: {type: Number, required: true},
+	tvshows:
+	[{
+		tvshowId: {type: Number, required: true},
 
-			screenplay: {type: Number, required: true},
-			pacing: {type: Number, required: true},
-			acting: {type: Number, required: true},
-			cinematography: {type: Number, required: true},
-			musicAndSound: {type: Number, required: true}
-		}],
-		tvshows:
-		[{
-			tvshowId: {type: Number, required: true},
-			
-			engagement: {type: Number, required: true},
-			consistency: {type: Number, required: true},
-			screenplay: {type: Number, required: true},
-			acting: {type: Number, required: true},
-			cinematography: {type: Number, required: true},
-			musicAndSound: {type: Number, required: true}
-		}]
-	}
+		status: {type: String, required: false},
+		venue: {type: String, required: false},
+		
+		ratings:
+		{
+			engagement: {type: Number, required: false},
+			consistency: {type: Number, required: false},
+			screenplay: {type: Number, required: false},
+			acting: {type: Number, required: false},
+			cinematography: {type: Number, required: false},
+			musicAndSound: {type: Number, required: false}
+		}
+	}],
+	movies:
+	[{
+		movieId: {type: Number, required: true},
+
+		status: {type: String, required: false},
+		venue: {type: String, required: false},
+		
+		ratings:
+		{
+			screenplay: {type: Number, required: false},
+			pacing: {type: Number, required: false},
+			acting: {type: Number, required: false},
+			cinematography: {type: Number, required: false},
+			musicAndSound: {type: Number, required: false}
+		}
+	}]
 })
 
 export default mongoose.model<UserType>('User', UserSchema)
