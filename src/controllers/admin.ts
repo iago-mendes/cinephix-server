c64db08292f47ca7f559adad899d6c745ac3a2f3
Iago <iagobrazmendes@gmail.com>
2021-03-20 12:17:17 -0300

    handle admin stats

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,9 +1,23 @@
 import {Request, Response} from 'express'
 
+import Group from '../models/Group'
+import User from '../models/User'
+
 const admin =
 {
 	stats: async (req: Request, res: Response) =>
-	{}
+	{
+		const users = await User.find()
+		const groups = await Group.find()
+
+		const stats =
+		{
+			numberOfUsers: users.length,
+			numberOfGroups: groups.length
+		}
+
+		return res.json(stats)
+	}
 }
 
 export default admin
